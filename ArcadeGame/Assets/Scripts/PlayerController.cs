7f3f3860014baa8bbdac18c74910f953db30ea3a
stager050522 <70406508+Anton050522@users.noreply.github.com>
2022-07-01 13:51:53 +0300

    add camera controller and fix jump
    add camera controller and fix jump

--- ArcadeGame/Assets/Scripts/PlayerController.cs
+++ ArcadeGame/Assets/Scripts/PlayerController.cs
@@ -6,20 +6,52 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     public float speed;
-    public float jumpheight;
-   
+    public float jumpForce;
+    private float moveInput;
+
+    private bool isGrounded;
+    public Transform groudChek;
+    public float chekRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJump;
+    public int extraJumpValue;
+
     void Start()
     {
+        extraJump = extraJumpValue;
         rb = GetComponent<Rigidbody2D>();
     }
     void FixedUpdate()
     {
-        Flip();
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
-        if (Input.GetKeyDown(KeyCode.Space))
-            rb.AddForce(transform.up * jumpheight, ForceMode2D.Impulse);
+        isGrounded = Physics2D.OverlapCircle(groudChek.position, chekRadius, whatIsGround);
 
+
+        Flip();
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+       
     }
+
+    private void Update()
+    {
+        if (isGrounded==true)
+        {
+            extraJump = extraJumpValue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && extraJump > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJump--;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && extraJump == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+       
+    }
+
     void Flip()
     {
         if (Input.GetAxis("Horizontal") > 0)
